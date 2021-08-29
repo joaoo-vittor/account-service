cb3434c4c286d739183b3e8a677b3b80bd1faf05
joaoo-vittor <joaoo.vittor007@gmail.com>
2021-08-29 19:32:55 -0300

    feature: implementing environment variables

---
+++ src/settings/__init__.py
@@ -0,0 +1,1 @@
+from .envs import SQLALCHEMY_DATABASE_URI
---
+++ src/settings/envs.py
@@ -0,0 +1,6 @@
+import os
+from dotenv import find_dotenv, load_dotenv
+
+load_dotenv(find_dotenv())
+
+SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI")

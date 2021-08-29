263c4daf2fab60c00de96f3e72d531a509171b38
joaoo-vittor <joaoo.vittor007@gmail.com>
2021-08-29 20:36:39 -0300

    feature: implementing interface to activate user

--- src/domain/use_cases/__init__.py
+++ src/domain/use_cases/__init__.py
@@ -1,3 +1,5 @@
 from .find_user import FindUser
 from .registry_user import RegistryUser
 from .updated_user import UpdateUser
+from .deactivate_user import DeactivateUser
+from .activate_user import ActivateUser
---
+++ src/domain/use_cases/activate_user.py
@@ -0,0 +1,15 @@
+from abc import ABC, abstractclassmethod
+from typing import Dict
+from src.domain.models import User
+
+
+class ActivateUser(ABC):
+    """Interface to ActivateUser use case"""
+
+    @abstractclassmethod
+    def activate_user(
+        cls, user_id: int, user_name: str, password: str
+    ) -> Dict[bool, User]:
+        """Specific Case"""
+
+        raise Exception("Should implement method: deactivate_user")

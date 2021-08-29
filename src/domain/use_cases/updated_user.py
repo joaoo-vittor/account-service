0e228fcea540b10a5497bf2f29afc0ff51b372cd
joaoo-vittor <joaoo.vittor007@gmail.com>
2021-08-29 19:35:33 -0300

    feature: implementing interface to updated user

---
+++ src/domain/use_cases/updated_user.py
@@ -0,0 +1,15 @@
+from abc import ABC, abstractclassmethod
+from typing import Dict
+from src.domain.models import User
+
+
+class UpdateUser(ABC):
+    """Interface to UpdateUser use case"""
+
+    @abstractclassmethod
+    def update_user(
+        cls, user_id: int, user_name: str, old_password: str, new_data: dict = None
+    ) -> Dict[bool, User]:
+        """Specific Case"""
+
+        raise Exception("Should implement method: update_user")

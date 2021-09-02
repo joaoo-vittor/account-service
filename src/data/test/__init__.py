57e76d378b8db34cca19f635a597fdb4aacfbd16
joaoo-vittor <joaoo.vittor007@gmail.com>
2021-09-02 17:22:54 -0300

    feature: Implementing activate and deactivate user spy

---
+++ src/data/test/__init__.py
@@ -0,0 +1,2 @@
+from .activate_user_spy import ActivateUserSpy
+from .deactivate_user_spy import DeactivateUserSpy
---
+++ src/data/test/deactivate_user_spy.py
@@ -0,0 +1,38 @@
+from typing import Type, Dict
+from src.domain.models import User
+from src.data.interface import UserRepositoryInterface as UserRepository
+
+
+class DeactivateUserSpy:
+    """Deactivate User Spy"""
+
+    def __init__(self, activate_user_repository: Type[UserRepository]) -> None:
+        self.activate_user_repository = activate_user_repository
+        self.deactivate_user_params = {}
+
+    def deactivate_user(
+        self, user_id: int, user_name: str, password: str
+    ) -> Dict[bool, User]:
+        """Deactivate User
+        :params - user_id: id of the user
+                - user_name: user name of User
+                - password: password to verify deactivate
+        :return - Dictionary with informations of the process
+        """
+
+        self.deactivate_user_params["user_id"] = user_id
+        self.deactivate_user_params["user_name"] = user_name
+        self.deactivate_user_params["password"] = password
+
+        response = None
+
+        validate_entry = (
+            isinstance(user_id, int)
+            and isinstance(user_name, str)
+            and isinstance(password, str)
+        )
+
+        if validate_entry:
+            response = User(id=1, user_name="", password="", email=None, active=0)
+
+        return {"Success": validate_entry, "Data": response}

c5bf0d037470f4d704288472003a1d2153d4ed3d
joaoo-vittor <joaoo.vittor007@gmail.com>
2021-09-02 17:12:56 -0300

    feature: Implementing validator to activate user query

---
+++ src/validators/__init__.py
@@ -0,0 +1,1 @@
+from .activate_user_route import validate_activate_user_route
---
+++ src/validators/activate_user_route.py
@@ -0,0 +1,12 @@
+from cerberus import Validator
+
+
+def validate_activate_user_route(query_params: any) -> bool:
+    schema = {
+        "user_id": {"type": "integer", "required": True},
+        "user_name": {"type": "string", "required": True},
+        "password": {"type": "string", "required": True},
+    }
+    v = Validator(schema)
+
+    return v.validate(query_params)

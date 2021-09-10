18474017d7329d7b256e3fbde6baf70480d3e116
joaoo-vittor <joaoo.vittor007@gmail.com>
2021-09-10 00:59:41 -0300

    improve: run server.

--- .flaskenv
+++ .flaskenv
@@ -1,2 +1,2 @@
-FLASK_APP=run.py
+FLASK_APP=server.py
 FLASK_ENV=development
--- server.py
+++ server.py
@@ -1,4 +1,8 @@
 from src.main.configs import api
 
-if __name__ == "__main__":
-    api.run(debug=True, host="0.0.0.0")
+# if __name__ == "__main__":
+#     api.run(debug=True, host="0.0.0.0")
+
+
+def create_app():
+    return api

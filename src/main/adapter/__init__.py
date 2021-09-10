3c43e2989820e3b42023a3d3ec2aff9c7c7b3eff
joaoo-vittor <joaoo.vittor007@gmail.com>
2021-09-10 00:14:43 -0300

    modularization.

--- src/main/adapter/__init__.py
+++ src/main/adapter/__init__.py
@@ -1,3 +1,5 @@
 from .api_adapter_registry_user import flask_adapter_registry_user
 from .api_adapter_find_user import flask_adapter_find_user
 from .api_adapter_update_user import flask_adapter_update_user
+from .api_adapter_deactivate_user import flask_adapter_deactivate_user
+from .api_adapter_activate_user import flask_adapter_activate_user

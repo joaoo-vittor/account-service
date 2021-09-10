63db26d885810dd5dda35dc94d4bd06241611c36
joaoo-vittor <joaoo.vittor007@gmail.com>
2021-09-09 22:07:28 -0300

    feature: implementing interface to updated user.

--- src/domain/use_cases/updated_user.py
+++ src/domain/use_cases/updated_user.py
@@ -7,9 +7,9 @@ class UpdateUser(ABC):
     """Interface to UpdateUser use case"""
 
     @abstractclassmethod
-    def update_user(
+    def updated_user(
         cls, user_id: int, user_name: str, old_password: str, new_data: dict = None
     ) -> Dict[bool, User]:
         """Specific Case"""
 
-        raise Exception("Should implement method: update_user")
+        raise Exception("Should implement method: updated_user")

11eeaafb45ce355f6b1a081d718779a16b55b757
joaoo-vittor <joaoo.vittor007@gmail.com>
2021-09-09 22:01:27 -0300

    modularization.

---
+++ src/presenters/controllers/__init__.py
@@ -0,0 +1,5 @@
+from .registry_user import RegistryUserController
+from .activate_user import ActivateController
+from .deactivate_user import DeactivateUserController
+from .find_user import FindUserController
+from .updated_user import UpdateUserController

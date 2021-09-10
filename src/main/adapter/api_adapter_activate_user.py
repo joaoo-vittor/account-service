5260a3a5c0cc3177fcf4ccb96555d039228d0363
joaoo-vittor <joaoo.vittor007@gmail.com>
2021-09-10 00:12:48 -0300

    feature: implementing adapter to activate user.

---
+++ src/main/adapter/api_adapter_activate_user.py
@@ -0,0 +1,51 @@
+from typing import Type
+from sqlalchemy.exc import IntegrityError
+from src.main.interface import RouteInterface as Route
+from src.presenters.helpes import HttpRequest, HttpResponse
+from src.presenters.errors import HttpErrors
+
+
+def flask_adapter_activate_user(request: any, api_route: Type[Route]) -> any:
+    """Adapter pattern to Flask
+    :param - Flask Request
+    :api_route - Composite Route
+    """
+
+    try:
+        query_string_params = request.get_json()
+
+        if (
+            "user_id" in query_string_params.keys()
+            and "user_name" in query_string_params.keys()
+            and "password" in query_string_params.keys()
+        ):
+            query_string_params["user_id"] = int(query_string_params["user_id"])
+            query_string_params["user_name"] = str(query_string_params["user_name"])
+            query_string_params["password"] = str(query_string_params["password"])
+
+    except:
+        http_error = HttpErrors.error_400()
+        return HttpResponse(
+            status_code=http_error["status_code"], body=http_error["body"]
+        )
+
+    http_request = HttpRequest(
+        header=request.headers, body=request.json, query=query_string_params
+    )
+
+    try:
+        response = api_route.route(http_request=http_request)
+    except IntegrityError:
+        http_error = HttpErrors.error_409()
+        return HttpResponse(
+            status_code=http_error["status_code"], body=http_error["body"]
+        )
+
+    except Exception as exc:
+        print(exc)
+        http_error = HttpErrors.error_500()
+        return HttpResponse(
+            status_code=http_error["status_code"], body=http_error["body"]
+        )
+
+    return response

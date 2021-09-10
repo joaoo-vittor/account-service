124ebc28f7842ff0f936916982dcc041e2918646
joaoo-vittor <joaoo.vittor007@gmail.com>
2021-09-09 22:14:53 -0300

    feature: implementing route to api.

---
+++ src/main/routes/api_route.py
@@ -0,0 +1,135 @@
+from flask import jsonify, request, Blueprint
+from werkzeug.security import check_password_hash
+from flask_jwt_extended import create_access_token
+from src.main.adapter import (
+    flask_adapter_registry_user,
+    flask_adapter_find_user,
+    flask_adapter_update_user,
+)
+from src.main.composer import (
+    registry_user_composite,
+    find_user_composite,
+    update_user_composite,
+)
+from src.settings import HASH
+
+api_route_bp = Blueprint("api_bp", __name__)
+
+
+@api_route_bp.route("/api/v1/registry", methods=["POST"])
+def register_user():
+    """register user route"""
+
+    message = {}
+
+    response = flask_adapter_registry_user(
+        request=request, api_route=registry_user_composite()
+    )
+
+    if response.status_code < 300:
+        message = {
+            "type": "users",
+            "id": response.body.id,
+            "attributes": {
+                "name": response.body.user_name,
+                "email": response.body.email,
+                "active": response.body.active,
+            },
+        }
+
+        return jsonify({"data": message}), response.status_code
+
+    # handling Errors
+    return (
+        jsonify(
+            {"error": {"status": response.status_code, "title": response.body["error"]}}
+        ),
+        response.status_code,
+    )
+
+
+@api_route_bp.route("/api/v1/login", methods=["POST"])
+def login_user():
+    """register user route"""
+
+    response = flask_adapter_find_user(request=request, api_route=find_user_composite())
+
+    if response.status_code < 300:
+        password = request.args.to_dict()["password"]
+        pwhash = f"{HASH}{response.body.password}"
+        valid_pw = check_password_hash(pwhash, password)
+
+        if valid_pw:
+            token_de_acesso = create_access_token(response.body.user_name)
+
+            message = {
+                "id": response.body.id,
+                "access_token": token_de_acesso,
+            }
+
+            return jsonify(message), response.status_code
+
+        return jsonify({"error": {"status": 401, "title": "Unauthorized"}}), 401
+
+    # handling Errors
+    return (
+        jsonify(
+            {"error": {"status": response.status_code, "title": response.body["error"]}}
+        ),
+        response.status_code,
+    )
+
+
+@api_route_bp.route("/api/v1/user_info", methods=["GET"])
+def user_info():
+    """register user route"""
+
+    response = flask_adapter_find_user(request=request, api_route=find_user_composite())
+
+    if response.status_code < 300:
+
+        message = {
+            "type": "users",
+            "id": response.body.id,
+            "attributes": {
+                "name": response.body.user_name,
+                "email": response.body.email,
+                "active": response.body.active,
+            },
+        }
+
+        return jsonify(message), response.status_code
+
+    # handling Errors
+    return (
+        jsonify(
+            {"error": {"status": response.status_code, "title": response.body["error"]}}
+        ),
+        response.status_code,
+    )
+
+
+@api_route_bp.route("/api/v1/update", methods=["POST"])
+def update_user():
+    """update user route"""
+
+    response = flask_adapter_update_user(
+        request=request, api_route=update_user_composite()
+    )
+
+    if response.status_code < 300:
+
+        message = {
+            "type": "users",
+            "updated": response.body,
+        }
+
+        return jsonify(message), response.status_code
+
+    # handling Errors
+    return (
+        jsonify(
+            {"error": {"status": response.status_code, "title": response.body["error"]}}
+        ),
+        response.status_code,
+    )

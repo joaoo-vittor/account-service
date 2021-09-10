22bad0d355a9e2957c082034e3155cc915e95a64
joaoo-vittor <joaoo.vittor007@gmail.com>
2021-09-09 22:05:44 -0300

    improve: fix user repository.

--- src/infra/repo/user_repository.py
+++ src/infra/repo/user_repository.py
@@ -1,8 +1,10 @@
+from werkzeug.security import generate_password_hash, check_password_hash
 from sqlalchemy.orm.exc import NoResultFound
 from src.data.interface import UserRepositoryInterface
 from src.domain.models import User
 from src.infra.config import DBConnectionHandler
 from src.infra.entites import Users as UserModel
+from src.settings import HASH
 
 
 class UserRepository(UserRepositoryInterface):
@@ -11,13 +13,17 @@ class UserRepository(UserRepositoryInterface):
     @classmethod
     def inserted_user(cls, user_name: str, password: str):
         """Insert data in user entity
-        :params - name: person name
-                - password: user password
+        :params
+                -- name: person name
+                -- password: user password
+
         :return - tuple with new user inserted
         """
 
         with DBConnectionHandler() as connection:
             try:
+                password = generate_password_hash(password=password).split(HASH)[1]
+
                 new_user = UserModel(user_name=user_name, password=password)
                 connection.session.add(new_user)
                 connection.session.commit()
@@ -29,6 +35,7 @@ class UserRepository(UserRepositoryInterface):
                     email=new_user.email,
                     active=new_user.active,
                 )
+
             except:
                 connection.session.rollback()
                 raise
@@ -40,8 +47,10 @@ class UserRepository(UserRepositoryInterface):
     @classmethod
     def selected_user(cls, user_name: str = None, user_id: int = None) -> User:
         """Select data in user entity by id and/or name
-        :params - user_id: Id of the registry
-                - user_name: User name
+        :params
+                -- user_id: Id of the registry
+                -- user_name: User name
+
         :return - Users selected
         """
         try:
@@ -94,15 +103,18 @@ class UserRepository(UserRepositoryInterface):
         new_data: dict = None,
     ) -> bool:
         """Update data of the user on database
-        :params - user_id: Id of the registry
-                - user_name: User name
-                - old_password: Old password to verify
-                - new_data: dict with new params to update
+        :params
+                -- user_id: Id of the registry
+                -- user_name: User name
+                -- old_password: Old password to verify
+                -- new_data: dict with new params to update
+
         :return - Boolean to Users updated
         """
         try:
-            if new_data is not None and (user_id and user_name and old_password):
+            if new_data and user_id and user_name and old_password:
                 with DBConnectionHandler() as connection:
+
                     user_verify = (
                         connection.session.query(UserModel)
                         .filter_by(id=user_id, user_name=user_name)
@@ -112,16 +124,19 @@ class UserRepository(UserRepositoryInterface):
                     if "active" in new_data.keys():
                         new_data.pop("active")
 
-                    if user_verify.password == old_password:
+                    pwhash = f"{HASH}{user_verify.password}"
+                    if check_password_hash(pwhash, old_password):
                         connection.session.query(UserModel).filter_by(
                             id=user_id, user_name=user_name
                         ).update(new_data, synchronize_session=False)
                         connection.session.commit()
+
                         return True
 
-            return None
+                return None
+
         except NoResultFound:
-            return None
+            return {}
         except:
             connection.session.rollback()
             raise
@@ -131,9 +146,11 @@ class UserRepository(UserRepositoryInterface):
     @classmethod
     def deactivate_user(cls, user_name: str, user_id: int, password: str) -> bool:
         """Deactivate user of database
-        :params - user_id: Id of the registry
-                - user_name: User name
-                - password: password to verify
+        :params
+                -- user_id: Id of the registry
+                -- user_name: User name
+                -- password: password to verify
+
         :return - Boolean to Users deactivate
         """
         try:
@@ -165,9 +182,11 @@ class UserRepository(UserRepositoryInterface):
     @classmethod
     def reactivate_user(self, user_name: str, user_id: int, password: str) -> bool:
         """Activate user of database
-        :params - user_id: Id of the registry
-                - user_name: User name
-                - password: password to verify
+        :params
+                -- user_id: Id of the registry
+                -- user_name: User name
+                -- password: password to verify
+
         :return - Boolean to Users activate
         """
         try:

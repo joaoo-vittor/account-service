4ca87bbdbf0e1e005cf178dc8df82709c5583619
joaoo-vittor <joaoo.vittor007@gmail.com>
2021-09-09 21:57:26 -0300

    improve: fix validator to find user route.

--- src/validators/find_user_route.py
+++ src/validators/find_user_route.py
@@ -3,8 +3,8 @@ from cerberus import Validator
 
 def validate_find_user_route(query_params: any) -> bool:
     schema = {
-        "user_id": {"type": "integer", "required": True},
-        "user_name": {"type": "string", "required": True},
+        "user_id": {"type": "integer", "required": False},
+        "user_name": {"type": "string", "required": False},
     }
     v = Validator(schema)
 
